0d1680e1125c7020aa91bfc06058d175bbc28736
Reynardt <recceray@gmail.com>
2024-05-28 10:36:35 +0200

    Saving
    Final product rock paper scissors game

--- script.js
+++ script.js
@@ -1,19 +1,62 @@
+let humanScore
+let computerScore
+
+/////////////////////////////////
+
 const getComputerChoice = () => {
-    const randomNumber = Math.random();
-    if (randomNumber < 1/3) {
-        return "rock";
-    } else if (randomNumber < 2/3) {
+    const randomNum = Math.random()
+
+    if (randomNum < 1/3 ) {
+        return "rock"
+    } else if (randomNum < 2/3 ) {
         return "paper"
     } else {
         return "scissors"
     }
-};
- const getHumanChoice = () => {
-    let choice = prompt("Enter your choice (rock, paper or scissors):").toLowerCase();
+}
+console.log(getComputerChoice());
+
+/////////////////////////////////
+
+const getHumanChoice = () => {
+    let choice = prompt("Enter your choice (rock, paper or scissors)").toLowerCase() 
+
     while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
-        choice = prompt("Invalid choice, enter rock paper or scissors").toLowerCase();
+        choice = prompt("Invalid choice. Please enter rock, paper or scissors.").toLowerCase()
     }
     return choice
- }
- 
- 
+}
+console.log(getHumanChoice())
+
+////////////////////////////////
+
+const playRound = (humanChoice, computerChoice) => {
+    humanChoice = humanChoice.toLowerCase()
+
+    let result = ""
+
+    if (humanChoice === computerChoice) {
+        result = "It's a tie"
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        result = `You win: ${humanChoice.charAt(0).toUpperase()} + ${humanChoice.slice(1)} + beats ${computerChoice}`
+        humanScore ++
+    } else {
+        result = `You lose: ${computerChoice.charAt(0).toUpperase()} + ${computerChoice.slice(1)} + beats ${humanChoice}`
+        computerScore ++
+    }
+}
+console.log(playRound())
+
+////////////////////////////////
+
+const playGame = () => {
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = humanChoice()
+        const computerSelection = computerChoice()
+        playRound(humanSelection, computerSelection)
+    }
+}
